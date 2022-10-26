dd095e7c0870b1844ab3f7a6bf5889b77a190bb1
MustafaSarikaya <msarikay@uottawa.ca>
2022-10-26 11:39:45 -0400

    remove changes out of scope

--- src/main/kotlin/seg3102team3/project/application/dtos/queries/PatientDto.kt
+++ src/main/kotlin/seg3102team3/project/application/dtos/queries/PatientDto.kt
@@ -1,7 +1,6 @@
 package seg3102team3.project.application.dtos.queries
 
 data class PatientDto (
-    val name: String
     ) {
     //unique identifier is string
 }
--- src/main/kotlin/seg3102team3/project/application/dtos/queries/PrescriptionDto.kt
+++ src/main/kotlin/seg3102team3/project/application/dtos/queries/PrescriptionDto.kt
@@ -1,7 +1,6 @@
 package seg3102team3.project.application.dtos.queries
 
 data class PrescriptionDto (
-    val drugInfo: DrugDto
         ){
     //todo include UUIDs for patient/prescriber/drug among other prescription info
 }
