8a1062f753ee6a9912b67461373acfc56a8021df
Gui <g.guy.lab@gmail.com>
2022-11-15 20:56:21 -0500

    Small fix to entity-update use case commands

--- src/main/kotlin/seg3102team3/project/application/usecases/PrivilegedUpdateAgent.kt
+++ src/main/kotlin/seg3102team3/project/application/usecases/PrivilegedUpdateAgent.kt
@@ -1,7 +1,8 @@
 package seg3102team3.project.application.usecases
 
 import seg3102team3.project.application.dtos.queries.AgentDto
+import java.util.*
 
 interface PrivilegedUpdateAgent {
-    fun privilegedUpdateAgent(updatedAgentInfo: AgentDto): Boolean
+    fun privilegedUpdateAgent(agentID: UUID, updatedAgentInfo: AgentDto): Boolean
 }
