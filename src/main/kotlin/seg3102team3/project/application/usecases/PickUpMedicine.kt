62fb955692cc08d12c3c9151fae2e826c383585d
Gui <g.guy.lab@gmail.com>
2022-11-16 01:08:28 -0500

    Minor consistency fix

--- src/main/kotlin/seg3102team3/project/application/usecases/PickUpMedicine.kt
+++ src/main/kotlin/seg3102team3/project/application/usecases/PickUpMedicine.kt
@@ -3,7 +3,7 @@ package seg3102team3.project.application.usecases
 import java.util.UUID
 
 interface PickUpMedicine {
-    fun pickUpMedicine(prescriptionFillId: UUID, pharmacistID: UUID, pickUpSummary: String): Boolean
+    fun pickUpMedicine(prescriptionFillID: UUID, pharmacistID: UUID, pickUpSummary: String): Boolean
     //pick up summary is updated from null to the new value in the chosen Prescription Fill's domain entity, status is set to retrieved
     //and the ID of the operating pharmacist is logged
 }
