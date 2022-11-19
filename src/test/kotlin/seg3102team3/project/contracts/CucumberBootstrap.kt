14a37b2f415a795743121b459ae1756d0ac84789
Guy L <g.guy.lab@gmail.com>
2022-11-18 19:32:06 -0500

    Tried hard to figure out how to run the tests, but ultimately failed

--- src/test/kotlin/seg3102team3/project/contracts/CucumberBootstrap.kt
+++ src/test/kotlin/seg3102team3/project/contracts/CucumberBootstrap.kt
@@ -1,4 +1,4 @@
-package seg3x02.auctionsystem.contracts
+package seg3102team3.project.contracts
 
 import io.cucumber.spring.CucumberContextConfiguration
 import org.springframework.boot.test.context.SpringBootTest
